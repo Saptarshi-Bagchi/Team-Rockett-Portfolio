dc8f27c0568f5f6bafb7906b39ffa4c5ae8f3b32
Saptarshi Bagchi <saptarshibagchi.05@gmail.com>
2026-09-22 19:57:23 +0530

    Updated Members

--- src/data/members.js
+++ src/data/members.js
@@ -5,7 +5,7 @@ const members = [
   { name: 'Saptarshi Bagchi', role: 'Software Developer', bio: 'I am Batman...', skills: ['ReactJS', 'SpringBoot', 'Python'], image: '/members/member-3.jpg', links: { github: 'https://github.com/Saptarshi-Bagchi', linkedin: 'https://www.linkedin.com/in/saptarshi-bagchi-104a26343/' } },
   { name: 'Shobhini Upadhyay', role: 'Public Speaker', bio: 'SoSo', skills: ['Team Management', 'Data Visualization', 'Audience Management'], image: '/members/member-4.jpg', links: { github: '#', linkedin: 'https://www.linkedin.com/in/shobhini-upadhyay-75635633b/' } },
   { name: 'Sayan Ghosh', role: 'UI/UX Developer', bio: 'Mr.sayan', skills: ['UI/UX', 'NodeJS', 'Python'], image: '/members/member-5.jpg', links: { github: '#', linkedin: 'https://www.linkedin.com/in/sayan-ghosh-71b268316/' } },
-  { name: 'Tejaswini Baral', role: 'Reels Enthusiast', bio: 'Zindgi ke sath bhi zindgi ke baad bhi', skills: ['Reels', 'Reels', 'Reels'], image: '/members/member-6.jpg', links: { github: '#', linkedin: 'https://www.linkedin.com/in/tejaswini-baral-41402339b/' } },
+  { name: 'Tejaswini Baral', role: 'Reels Enthusiast', bio: 'Zindgi ke sath bhi zindgi ke baad bhi', skills: ['Reels', 'Reels', 'Reels'], image: '/members/member-6.jpg', links: { github: 'https://github.com/tejaswini-baral', linkedin: 'https://www.linkedin.com/in/tejaswini-baral-41402339b/' } },
 ]
 
 export default members
